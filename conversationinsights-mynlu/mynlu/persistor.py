c47fe5b55e1f3227dd4513a0db14edb662e59ba1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 04:31:45 +0000

    Adding tarfile member sanitization to extractall()

--- conversationinsights-mynlu/mynlu/persistor.py
+++ conversationinsights-mynlu/mynlu/persistor.py
@@ -76,4 +76,23 @@ class CloudPersistor(Persistor):
         with io.open(filename, 'wb') as f:
             self.bucket.download_fileobj(filename, f)
         with tarfile.open(filename, "r:gz") as tar:
-            tar.extractall(self.data_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, self.data_dir)
